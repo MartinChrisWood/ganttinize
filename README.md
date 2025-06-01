806b0668a04dc4cb963f6dffccb8e14661b5df54
Martin Wood <martin.chris.wood@gmail.com>
2025-06-01 15:54:04 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ganttinize
-Gantt chart generation using Plotly
+
+Gantt chart generation using Plotly, following [this Plotly guide](https://plotly.com/python/gantt/)
+
+And then adding some bits.
+
+## Setup
+```bash
+python -m venv env
+source env/bin/activate
+
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+## Run
+
+This is very barebones, code reads `example.csv`, which defines the tasks to plot
+with task names, start dates, finish datee, a list of dependencies (just a text field for now)
+and "Resource" (that's humans, to the rest of us).
+
+```bash
+python gantt.py
+```
+
+## To be added if I remember
+
+- I want to instead define a start date, dependencies and weeks taken for tasks and have the software figure out the task start dates for me.
---
+++ gantt.py
@@ -0,0 +1,47 @@
+import plotly.express as px
+import plotly.graph_objects as go
+
+import numpy as np
+import pandas as pd
+
+from dash import Dash, dcc, html
+
+
+# Load the data (see section above)
+df = pd.read_csv("example.csv", dtype=str)
+
+# Create the labelling for the task blocks (I want to see task dependencies)
+df['Labelling'] = np.where(
+    df['Dependencies'].isnull(),
+    df['Task'],
+    df['Task'] + ", Dependencies: " + df["Dependencies"]
+)
+
+# Order as I want plotted
+df = df.sort_values("Start", ascending=False)
+
+# Plot
+fig = px.timeline(
+    df,
+    x_start="Start",
+    x_end="Finish",
+    y="Task",
+    color="Resource",
+    text="Labelling",
+    hover_name="Resource",
+    hover_data={"Resource": False, "Start": True, "Finish": True, "Labelling": False},
+    title="Gantt Chart"
+)
+
+fig.show()
+
+# # Wraps Dash around the plot
+# fig = go.Figure(fig)
+
+# # Dash stuff, create the app and start it
+# app = Dash()
+# app.layout = html.Div([
+#     dcc.Graph(figure=fig)
+# ])
+
+# app.run_server(debug=True, use_reloader=True)
